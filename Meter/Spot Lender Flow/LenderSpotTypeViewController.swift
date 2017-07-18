df126025aedd60302af994767d0b1b6102d99c12
Avery Lamp <averylamp@gmail.com>
2017-07-18 00:58:15 -0400

    Spot type page added

--- Meter/Spot Lender Flow/LenderSpotTypeViewController.swift
+++ Meter/Spot Lender Flow/LenderSpotTypeViewController.swift
@@ -9,18 +9,44 @@
 import UIKit
 
 class LenderSpotTypeViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
+    let spotTypes:[String] = ["Driveway", "Curbside", "Garage", "Parking Lot", "Alley", "Tandem"]
     @IBAction func learnMoreClicked(_ sender: Any) {
         
     }
     
+    
+}
+
+extension LenderSpotTypeViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return spotTypes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "SpotTypeCell", for: indexPath) as? SpotTypeTableViewCell{
+            cell.typeLabel.text = spotTypes[indexPath.row]
+            cell.tag = indexPath.row
+            return cell
+        }else{
+            print("Error creating Spot Type Cell")
+            return UITableViewCell()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
     
 }
